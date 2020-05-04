f91a7a848534807dd8a8804688c94e6a6fb4151f
haerim95 <tjffldi123@daum.net>
2020-05-04 23:51:34 +0900

    about year 작업중

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@ const $btnScrollTop = document.querySelector('.topUp');
 function scrollUp(){
     if(window.pageYOffset === 0){
         clearInterval(scrollTopValue);
-    }else if(window.pageXOffset >= 400){
-        $btnScrollTop.classList.toggle('hide');
     }
     window.scroll(0, window.pageYOffset - 50);
 }
@@ -13,3 +11,9 @@ function scrollToTop(){
     scrollTopValue = setInterval(scrollUp, 16.66);
 }
 $btnScrollTop.addEventListener('click', scrollToTop);
+// top button hide / show
+$btnScrollTop.style.visibility = "hidden";
+window.onscroll = function() {
+    var pageOffset = document.documentElement.scrollTop || document.body.scrollTop;
+    if ($btnScrollTop) $btnScrollTop.style.visibility = pageOffset > 100 ? 'visible' : 'hidden';
+};
