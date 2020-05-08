0ddd1ab978ccffce31fff47f9f637adf508f4006
haerim95 <tjffldi123@daum.net>
2020-05-08 22:39:26 +0900

    modify

--- js/bg.js
+++ js/bg.js
@@ -6,30 +6,3 @@ function bgImgChange(){
 }
 bgImgChange();
     
-// const bgimages = document.querySelector("main");
-
-// const IMG_NUMBER = 5;
-
-// function loadend(){
-//     console.log("finishedLoad")
-// }
-
-// function panintImage(imgNumber){
-//     const image = new Image();
-//     // +1 을 주는 이유 = Math.random() 함수가 0을 줄 수 있기 때문
-//     image.src=`/image/main/${imgNumber + 1}.jpg`;
-//     image.classList.add("bgImg");
-//     bgimages.prepend(image);
-// }
-
-// function genRandom(){
-//     //math.floor는 버림, ceiling은 올림. 둘다 소수쩜 삭제
-//     const number = Math.floor(Math.random() * IMG_NUMBER);
-//     return number;
-// }
-
-// function init(){
-//     const randomNumber = genRandom();
-//     panintImage(randomNumber);
-// }
-// init();
